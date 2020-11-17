34a8c9e854f1b7957c800e09cd588100e6be27a0
Patrick Touchette <pat_touchette@hotmail.com>
2020-11-17 15:30:40 -0500

    output resized images

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 const fs = require('fs');
+const path = require('path');
 const sharp = require('sharp');
 
-sharp('./images/canoe.jpg')
-  .resize(1500)
-  .toFile('./output/test.jpg', (err, info) => {
-    if (err) {
-      console.log('err', err);
-    }
-    console.log(info);
+// Set your image parameters
+const inputFolder = './images';
+const outputFolder = './output';
+const imageWidth = 1200;
+
+//joining path of directory
+const directoryPath = path.join(__dirname, inputFolder);
+
+// Get all files in directory
+fs.readdir(directoryPath, (err, files) => {
+  if (err) console.log(err);
+
+  //listing all files using forEach
+  files.forEach((file) => {
+    // Resize file
+    sharp(`./${inputFolder}/${file}`)
+      .resize(imageWidth)
+      .toFile(`./${outputFolder}/${file}`, (err, info) => {
+        if (err) console.log('err', err);
+        console.log(info);
+        console.log(`${file} ... done`);
+      });
   });
+});
---
+++ readme.md
@@ -0,0 +1,23 @@
+# Nodejs Sharp Image resizer
+
+Simple script to resize all the images inside a folder. You may specify the desired width. New files are created in an output folder.
+
+## Installation
+
+Make sure you have node.js installed.
+Clone this repo, then install the dependencies.
+
+`npm install`
+
+## Usage
+
+`node app.js`
+
+## Set some basic parameters
+
+```js
+// Set your image parameters
+const inputFolder = './images';
+const outputFolder = './output';
+const imageWidth = 1200;
+```
